945b70218edbf655cee9154a898f4504c5948d57
Amanuel Tamirat <Amanuel_Tamirat@sch.local>
2026-02-27 09:18:46 +0300

    updates

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,12 +1,15 @@
 import { useRef, useState } from "react"
 import { motion,useScroll, useSpring, useTransform } from "framer-motion";
-import {FaArrowDown} from "react-icons/fa";
+import {FaArrowDown, FaArrowRight} from "react-icons/fa";
 import { service } from "./servise";
+import { useNavigate } from "react-router-dom";
 
 
 function Service() {
 
 const [activeIndex, setActiveIndex] = useState(null)
+
+const navigate = useNavigate()
 
   return (
     <div   className="home-service flex gap-20 mx-20  mt-20">
@@ -25,11 +28,11 @@ const [activeIndex, setActiveIndex] = useState(null)
               <div onClick={() => setActiveIndex(item.id)} key={item.id} className={`${activeIndex == item.id ? "border-orange-800": "border-gray-700"} text-black border bottom-2  hover:box hover:item-desc active-desc`}>
           <div className="flex justify-between px-4 py-8 items-center content  ">
           <h1 className="text-2xl text-black tracking-[0.1rem]">{item.title}</h1>
-           <FaArrowDown className= {`${activeIndex == item.id ? 'icons': ''}`} color='gray' size={14}/>
+           <FaArrowRight onClick={()=>navigate(item.to)} className= {`cursor-pointer`} color='black' size={18}/>
           </div>
-          <div className={`${activeIndex == item.id ? "block" : "hidden"}  text-gray-400 p-5 tracking-[0.05rem]`}>
+          {/* <div className={`${activeIndex == item.id ? "block" : "hidden"}  text-gray-400 p-5 tracking-[0.05rem]`}>
              <p className={` item-desc active-desc font-serif`}>{item.desc}</p>
-          </div>
+          </div> */}
         </div>
             )
           })
--- src/components/servise.js
+++ src/components/servise.js
@@ -1,30 +1,34 @@
 
-
 export const service = [
     {
         id:1,
         title:"Custom Software Development",
+        to: '/services/custom-software-development',
         desc:'Custom software development focuses on creating tailor-made solutions that meet the unique needs of a business. It involves designing, developing, and deploying software applications specifically built to solve specific challenges. By leveraging custom-built solutions, businesses can improve efficiency, scalability, and overall performance.'
     },
      {
         id:2,
         title:"Web Development",
+        to: '/services/web-development',
         desc:"Our Agile experts can create bespoke applications, platforms and products to meet the requirements of your business or new venture."
     },
      {
         id:3,
         title:"Mobile Development",
+        to: '/services/mobile-development',
         desc:"We deliver more than just apps. We deliver experiences. Our fast, responsive and highly scalable mobile app development will help you get ahead of the curve in no time!"
     },
     
      {
         id:4,
-        title:"Webflow",
+        title:"UI/UX Design",
+        to: '/services/ui-ux-design-services',
         desc:"Webflow is a very popular website builder and CMS today, it offers a user-friendly interface, a wide range of design templates, and powerful customization options, making it a great choice for both beginners and experienced web designers. We are working with this CMS since its appearance, so our developers are really masters here."
     },
      {
         id:5,
-        title:"E-commerce",
+        title:"Technical Support",
+        to: '/services/technical-support',
         desc:"We create E-commerce websites which provide a robust platform for businesses to sell their products or services online, allowing customers to make purchases from the comfort of their own homes. WooCommerce, Shopify or PrestaShop would be perfect solution for most of the businesses."
     }
 ]
