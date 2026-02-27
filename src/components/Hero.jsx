03911b9180ce65ba3fac1683a6f451a3367396a7
Amanuel Tamirat <Amanuel_Tamirat@sch.local>
2026-02-27 09:00:35 +0300

    new update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,22 +5,22 @@ function Hero() {
 
 
   return (
-    <div className='flex justify-between px-20 mb-10 pt-52 bg-[#040404] shadow-lg shadow-[#06f3ffb8]'>
+    <div className='flex justify-between px-20 mb-10 pt-52 bg-[#e6e6e6] shadow-sm shadow-[#363a3ab8]'>
       <div className='flex flex-col'>
         <div className='hero-sec'>
-        <h1 className='uppercase text-white font-serif font-semibold w-[700px] text-6xl'>
+        <h1 className='uppercase text-black font-serif font-semibold w-[700px] text-6xl'>
         Your digital agency <br /> in analog world
         </h1>
         </div>
         <div>
-            <p className=' mt-20 italic text-xl'>Write to us</p>
-            <a className='text-[#06ffff] text-lg font-semibold ' href="#">contact@erazonetech.com</a>
+            <p className=' mt-20 italic text-xl text-black'>Write to us</p>
+            <a className='text-[#343636] text-lg font-semibold ' href="#">contact@erazonetech.com</a>
         </div>
       </div>
       <div className="flex relative">
        <video className=' w-[40rem]'
          width="" height="500"  autoPlay loop muted >
-         <source src="https://bato.dev/wp-content/uploads/2023/04/main-video-min-1.mp4" type="video/mp4"/>
+         <source src="/my-video.mp4" type="video/mp4"/>
      </video>
       </div>
     </div>
